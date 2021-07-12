19d43631931f56f154f1024184be73b28e65d2b2
Govardhan-github <govardhanbajjuri1998@gmail.com>
2021-07-12 07:12:27 -0700

    UC2 Checking equal lines are not

--- src/lengthcal.java
+++ src/lengthcal.java
@@ -3,21 +3,34 @@ public class lengthcal {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to line comparision computation programme");
-				int X1,X2,Y1,Y2;
-				double length;
+				int X1,X2,Y1,Y2,A1,A2,B1,B2;
+				double length1,length2;
 				System.out.println("enter X1 value ");
 				System.out.println("enter X2 value ");
 				System.out.println("enter Y1 value ");
 				System.out.println("enter Y2 value ");
+				System.out.println("enter A1 value ");
+				System.out.println("enter A2 value ");
+				System.out.println("enter B1 value ");
+				System.out.println("enter B2 value ");
 				Scanner ref = new Scanner(System.in);
 				X1=ref.nextInt();
 				X2=ref.nextInt();
 				Y1=ref.nextInt();
 				Y2=ref.nextInt();
-				length=Math.sqrt(((X2-X1)*(X2-X1))+((Y2-Y1)*(Y2-Y1)));
-				System.out.println("LENGTH OF LINE "+length);
-		
-		
+				A1=ref.nextInt();
+				A2=ref.nextInt();
+				B1=ref.nextInt();
+				B2=ref.nextInt();
+				length1=Math.sqrt(((X2-X1)*(X2-X1))+((Y2-Y1)*(Y2-Y1)));
+				length2=Math.sqrt(((A2-A1)*(A2-A1))+((B2-B1)*(B2-B1)));
+				System.out.println("LENGTH OF LINE1 "+length1);
+				System.out.println("LENGTH OF LINE2 "+length2);
+				
+				if (length1 == length2)
+					System.out.println("Two lines are equal");
+				else
+					System.out.println("Two lines are not equal");
 		
 	}
 
